d5e8303597a7355d041c1c0de50ef80fb3bb6517
darren-a <darren_adkinson@hotmail.com>
2018-09-13 16:34:30 -0700

    Initial commit go through Warmup

---
+++ musicapp/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  # Create a SessionsController but no Session model.
+  #   - Write controller methods and the accompanying routes so that users
+  #     can log in and log out. Should session be a singular resource?
+  #   - SessionsController#create should re-set the appropriate user's
+  #     session_token and session[:session_token].
+  #   - For now just redirect them to a User#show page which simply displays
+  #     that user's email.
+
+
+  def new
+
+  end
+
+
+  def create
+
+  end
+
+
+  def destroy
+
+  end
+
+end
